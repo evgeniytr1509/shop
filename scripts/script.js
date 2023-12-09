2304fa65b019fa1f503e1de5e1609d9480d6477a
evgeniytr1509 <evgeniytr1509@gmail.com>
2023-12-09 16:25:06 +0200

    Second1

--- scripts/script.js
+++ scripts/script.js
@@ -86,7 +86,7 @@ document.getElementById("change-currency").onclick = function (e) {
 
     else if (currentCurrency === "₽") {
         newCurrency = "BYN";
-        coefficient = 13;
+        coefficient = 3;
 
     }
     else if (currentCurrency === "BYN") {
