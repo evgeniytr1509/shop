478bbff77256b92827d289bfc163bef6c1a082f6
evgeniytr1509 <evgeniytr1509@gmail.com>
2023-12-09 16:22:56 +0200

    First1

--- scripts/script.js
+++ scripts/script.js
@@ -86,7 +86,7 @@ document.getElementById("change-currency").onclick = function (e) {
 
     else if (currentCurrency === "₽") {
         newCurrency = "BYN";
-        coefficient = 3;
+        coefficient = 2;
     }
     else if (currentCurrency === "BYN") {
         newCurrency = "¥";
